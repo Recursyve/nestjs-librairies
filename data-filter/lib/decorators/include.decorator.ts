22b70734b7e12e288fbe9387863fd85d0325d064
Gabriel Beaudry <gabriel@recursyve.io>
2020-03-05 11:20:03 -0500

    Update include.decorator.ts

--- data-filter/lib/decorators/include.decorator.ts
+++ data-filter/lib/decorators/include.decorator.ts
@@ -1,8 +1,13 @@
 import { INCLUDE } from "../constant";
 import { IncludeConfig } from "../models/include.model";
 
-export function Include(options: IncludeConfig): PropertyDecorator {
+export function Include(options: IncludeConfig | string): PropertyDecorator {
     return (target: Object, propertyKey: string) => {
+        // Simplify just passing the path.
+        if (typeof options === "string") {
+            options = { path: options };
+        }
+        
         const includes: IncludeConfig[] =
             Reflect.getMetadata(INCLUDE.replace("{{include}}", propertyKey), target) || [];
         includes.push(options);
