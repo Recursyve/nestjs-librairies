86450f1f79d1313ec05624d0e7e6b5bcd9cb1de6
JulienDuf <julien@recursyve.io>
2020-03-09 14:16:50 -0400

    Cleanup constants

--- data-filter/lib/constant.ts
+++ data-filter/lib/constant.ts
@@ -1,19 +1,2 @@
 export const ATTRIBUTES = "__ATTRIBUTES__";
 export const DATA_FILTER = "__DATA_FILTER__";
-export const CUSTOM_ATTRIBUTES = "__CUSTOM_ATTRIBUTES{{attribute}}__";
-export const MODEL_ATTRIBUTES = "__MODEL_ATTRIBUTES__";
-export const MODEL_CUSTOM_ATTRIBUTES = "__MODEL_CUSTOM_ATTRIBUTES__";
-export const HAVING = "__HAVING__";
-export const GROUP = "__GROUP__";
-export const MODEL = "__MODEL__";
-export const EXPORTS = "__EXPORTS__";
-export const PATH = "__PATH{{path}}__";
-export const INCLUDE = "__INCLUDE{{include}}__";
-
-export const CONSTANTS = {
-    having: HAVING,
-    group: GROUP,
-    model: MODEL,
-    path: PATH,
-    include: INCLUDE
-};
