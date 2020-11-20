6793f0619e35c183d7e868497be057096f80e804
Julien Dufresne <julien@recursyve.io>
2020-11-20 16:15:37 -0500

    Added support for connection string (#23)

--- redis/lib/services/redis-config.service.ts
+++ redis/lib/services/redis-config.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import * as Redis from "ioredis";
 import { RedisOptions } from "ioredis";
 
 @Injectable()
 export class RedisConfigService {
+    public connectionString: string;
     public host: string;
     public port: number;
     public password: string;
     public database: number;
 
     constructor() {
+        this.connectionString = process.env.REDIS_CONNECTION_STRING;
         this.host = process.env.REDIS_HOST ?? "127.0.0.1";
         this.password = process.env.REDIS_PASSWORD;
         this.port = +process.env.REDIS_PORT ?? 6379;
@@ -27,5 +30,13 @@ export class RedisConfigService {
         }
 
         return option;
+    }
+
+    public getRedisInstance(): Redis.Redis {
+        const options = this.getConfig();
+        if (this.connectionString) {
+            return new Redis(this.connectionString, options);
+        }
+        return new Redis(options);
     }
 }
