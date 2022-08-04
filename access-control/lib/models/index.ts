8a98c25cde663f146ec06253c29e188e12d2c34f
JulienDuf <julien@recursyve.io>
2022-08-04 17:55:01 -0400

    Fix exports

--- access-control/lib/models/index.ts
+++ access-control/lib/models/index.ts
@@ -1,5 +1,6 @@
 export * from "./access-control-objects.model";
 export * from "./access-rules.model";
+export * from "./deleted-resources.model";
 export * from "./resources.model";
 export * from "./user-resources.model";
 export * from "./users.model";
