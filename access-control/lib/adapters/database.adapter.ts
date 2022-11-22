65360e16f2717380fed8936d163313beb0e6c6c4
Julien Dufresne <julien@recursyve.io>
2022-11-22 11:52:18 -0500

    Fix for resource ids (#80)
    * Fix for resource ids
    
    * Use Database adapter to parse ids

--- access-control/lib/adapters/database.adapter.ts
+++ access-control/lib/adapters/database.adapter.ts
@@ -3,5 +3,6 @@ import { ResourceId } from "../models";
 export interface IDatabaseAdapter {
     getModels(): any[];
     getResourceName(model: any): string;
+    parseIds(model: any, ids: string | string[]): ResourceId | ResourceId[];
     checkIfResourceExist(model: any, resourceId: ResourceId, condition: any): Promise<any>;
 }
