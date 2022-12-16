98c554e9d1c8778b212854b7944004af5c294cb2
Jtplouffe <32107801+Jtplouffe@users.noreply.github.com>
2022-12-16 13:06:02 -0500

    Fix access control guard for mongoose (#82)

--- access-control/lib/decorators/access-control-resource.decorator.ts
+++ access-control/lib/decorators/access-control-resource.decorator.ts
@@ -2,9 +2,10 @@ import { SetMetadata } from "@nestjs/common";
 import { AccessControlResourceConfig } from "../models/access-control-resource.model";
 import { ACCESS_CONTROL_RESOURCE } from "./constant";
 
-export const AccessControlResource = (model: any, paramId = "id") => {
+export const AccessControlResource = (model: any, paramId = "id", type?: string) => {
     return SetMetadata(ACCESS_CONTROL_RESOURCE, {
         model,
-        paramId
+        paramId,
+        type
     } as AccessControlResourceConfig);
 };
