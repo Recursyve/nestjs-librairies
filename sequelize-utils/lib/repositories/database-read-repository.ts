051a51c9b1d102b1f35c5170f90218cb88765095
JulienDuf <julien@recursyve.io>
2023-09-30 11:09:37 -0400

    Fix to support strict types for sequelize

--- sequelize-utils/lib/repositories/database-read-repository.ts
+++ sequelize-utils/lib/repositories/database-read-repository.ts
@@ -1,22 +1,23 @@
 import { CountOptions, FindOptions, Identifier } from "sequelize";
+import { Attributes } from "sequelize/types/model";
 import { SequelizeEntities } from "../models/sequelize-entities.model";
 
-export class SequelizeReadRepository<T extends SequelizeEntities> {
-    constructor(protected repository: typeof SequelizeEntities) {}
+export abstract class SequelizeReadRepository<T extends SequelizeEntities> {
+    protected abstract repository: typeof SequelizeEntities<any, any>;
 
-    public findByPk(identifier: Identifier, options?: FindOptions): Promise<T | null> {
+    public findByPk(identifier: Identifier, options?: FindOptions<Attributes<T>>): Promise<T | null> {
         return this.repository.findByPk(identifier, options) as unknown as Promise<T | null>;
     }
 
-    public findOne(options?: FindOptions): Promise<T | null> {
+    public findOne(options?: FindOptions<Attributes<T>>): Promise<T | null> {
         return this.repository.findOne(options) as unknown as Promise<T | null>;
     }
 
-    public findAll(options?: FindOptions): Promise<T[]> {
+    public findAll(options?: FindOptions<Attributes<T>>): Promise<T[]> {
         return this.repository.findAll(options) as unknown as Promise<T[]>;
     }
 
-    public count(options?: CountOptions): Promise<number> {
+    public count(options?: CountOptions<Attributes<T>>): Promise<number> {
         return this.repository.count(options);
     }
 }
