16b17814294859e6d3264a2ee62c6f5db870fd98
JulienDuf <julien@recursyve.io>
2024-04-26 16:41:54 -0400

    [Sequelize Utils] Add AfterBulkCreateFormatDateOnlyHook

--- sequelize-utils/lib/hooks/after-create-format-date-only.hook.ts
+++ sequelize-utils/lib/hooks/after-create-format-date-only.hook.ts
@@ -1,6 +1,12 @@
-import { CreateOptions, Model } from "sequelize";
+import { BulkCreateOptions, CreateOptions, Model } from "sequelize";
 import { HookReturn } from "sequelize/types/hooks";
 import { SequelizeUtils } from "../utils/sequelize.utils";
+
+export function AfterBulkCreateFormatDateOnlyHook<M extends Model>(instances: M[], options: BulkCreateOptions): HookReturn {
+    for (const instance of instances) {
+        AfterCreateFormatDateOnlyHook(instance, options);
+    }
+}
 
 export function AfterCreateFormatDateOnlyHook<M extends Model>(instance: M, options: CreateOptions): HookReturn {
     if (!instance) {
--- sequelize-utils/lib/sequelize-utils.module.ts
+++ sequelize-utils/lib/sequelize-utils.module.ts
@@ -1,11 +1,14 @@
 import { Global, Module, OnModuleInit } from "@nestjs/common";
 import { Sequelize } from "sequelize-typescript";
-import { AfterCreateFormatDateOnlyHook } from "./hooks/after-create-format-date-only.hook";
+import {
+    AfterBulkCreateFormatDateOnlyHook,
+    AfterCreateFormatDateOnlyHook
+} from "./hooks/after-create-format-date-only.hook";
 import { AfterFindFormatDateOnlyHook } from "./hooks/after-find-format-date-only.hook";
 import { SequelizeTransactionService } from "./transactions/sequelize-transaction.service";
 
+/** @deprecated: Use SequelizeUtilsModule instead */
 @Module({})
-/** @deprecated Use SequelizeUtilsModule instead */
 export class SequelizeUtilsHooksModule implements OnModuleInit {
     constructor(private sequelize: Sequelize) {}
 
@@ -24,6 +27,7 @@ export class SequelizeUtilsModule implements OnModuleInit {
     constructor(private sequelize: Sequelize) {}
 
     public onModuleInit(): void {
+        this.sequelize.addHook("afterBulkCreate", "transform-date-only", AfterBulkCreateFormatDateOnlyHook);
         this.sequelize.addHook("afterCreate", "transform-date-only", AfterCreateFormatDateOnlyHook);
         this.sequelize.addHook("afterFind", "transform-date-only", AfterFindFormatDateOnlyHook);
     }
