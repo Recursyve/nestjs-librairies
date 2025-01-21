681ebf0ba668de1b2f2028f178f34ddfd1f7581d
JulienDuf <julien@recursyve.io>
2025-01-21 16:36:53 -0500

    feat: [SequelizeUtils] new options in transaction and afterUpdate hooks

--- sequelize-utils/lib/transactions/sequelize-transaction.service.ts
+++ sequelize-utils/lib/transactions/sequelize-transaction.service.ts
@@ -3,27 +3,40 @@ import { TransactionOptions } from "sequelize";
 import { Sequelize } from "sequelize-typescript";
 import { NiceTransaction } from "./transaction";
 
+export interface NiceTransactionOptions extends TransactionOptions {
+    transaction?: NiceTransaction;
+    useSavePoints?: boolean;
+}
+
 @Injectable()
 export class SequelizeTransactionService {
     @Inject()
     private sequelize!: Sequelize;
 
-    public transaction<T>(options: TransactionOptions, autoCallback: (t: NiceTransaction) => PromiseLike<T>): Promise<T>;
+    public transaction<T>(options: NiceTransactionOptions, autoCallback: (t: NiceTransaction) => PromiseLike<T>): Promise<T>;
     public transaction<T>(autoCallback: (t: NiceTransaction) => PromiseLike<T>): Promise<T>;
-    public transaction(options?: TransactionOptions): Promise<NiceTransaction>;
-    public async transaction<T>(optionsOrAutoCallback?: TransactionOptions | ((t: NiceTransaction) => PromiseLike<T>), _autoCallback?:(t: NiceTransaction) => PromiseLike<T>): Promise<T | NiceTransaction> {
-        let options: TransactionOptions = {};
+    public transaction(options?: NiceTransactionOptions): Promise<NiceTransaction>;
+    public async transaction<T>(optionsOrAutoCallback?: NiceTransactionOptions | ((t: NiceTransaction) => PromiseLike<T>), _autoCallback?:(t: NiceTransaction) => PromiseLike<T>): Promise<T | NiceTransaction> {
+        let options: NiceTransactionOptions = {};
         let autoCallback: ((t: NiceTransaction) => PromiseLike<T>) | null = null;
 
         if (_autoCallback) {
             autoCallback = _autoCallback;
-            options = optionsOrAutoCallback as TransactionOptions;
+            options = optionsOrAutoCallback as NiceTransactionOptions;
         }
 
         if (optionsOrAutoCallback instanceof Function) {
             autoCallback = optionsOrAutoCallback as (t: NiceTransaction) => PromiseLike<T>;
         } else {
-            options = optionsOrAutoCallback as TransactionOptions;
+            options = optionsOrAutoCallback as NiceTransactionOptions;
+        }
+
+        if (options.transaction && !options.useSavePoints) {
+            if (!autoCallback) {
+                return options.transaction;
+            }
+
+            return await autoCallback(options.transaction);
         }
 
         const transaction = new NiceTransaction(this.sequelize, options);
