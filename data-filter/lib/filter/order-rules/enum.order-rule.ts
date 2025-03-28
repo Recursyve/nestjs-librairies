cd344e37a441a95cfed9ad2a19d579a0d0b03929
Gabriel Roberge <groberge@recursyve.io>
2025-03-28 17:04:53 -0400

    Fix order option string interpolation in EnumOrderRule for correct model attribute reference

--- data-filter/lib/filter/order-rules/enum.order-rule.ts
+++ data-filter/lib/filter/order-rules/enum.order-rule.ts
@@ -14,7 +14,7 @@ export class EnumOrderRule extends OrderRule implements EnumOrderRuleDefinition 
     }
 
     public getOrderOption(model: typeof M): OrderItemColumn {
-        let order = `CASE ${this.path ? SequelizeUtils.getLiteralFullName(this.attribute, this.path) : this.attribute} `;
+        let order = `CASE ${this.path ? SequelizeUtils.getLiteralFullName(this.attribute, this.path) : `\`${model.name}\`.\`${this.attribute}\``} `;
         for (let i = 0; i < this.priorities.length; ++i) {
             order += `WHEN '${this.priorities[i]}' THEN ${this.priorities.length - i} `;
         }
