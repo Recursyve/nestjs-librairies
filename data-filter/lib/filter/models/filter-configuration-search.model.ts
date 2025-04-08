a3ff7b86e8b659ee6c1d4c1a19073f2682e2f71f
JulienDuf <julien@recursyve.io>
2025-04-07 20:19:07 -0400

    chore(data-filter): add class-validator decorators to support Global ValidationPipe usage

--- data-filter/lib/filter/models/filter-configuration-search.model.ts
+++ data-filter/lib/filter/models/filter-configuration-search.model.ts
@@ -1,15 +1,20 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { Expose } from "class-transformer";
 
 export class FilterConfigurationSearchModel {
+    @Expose()
     @ApiProperty({ type: () => String })
     id!: string;
 
+    @Expose()
     @ApiProperty({ type: () => Object })
     value!: unknown;
 
+    @Expose()
     @ApiPropertyOptional({ type: () => Number })
     pageSize?: number;
 
+    @Expose()
     @ApiPropertyOptional({ type: () => Number })
     page?: number;
 }
--- data-filter/lib/filter/models/query.model.ts
+++ data-filter/lib/filter/models/query.model.ts
@@ -1,19 +1,23 @@
 import { ApiExtraModels, ApiProperty, getSchemaPath } from "@nestjs/swagger";
+import { IsArray, IsIn, IsString } from "class-validator";
 import { RuleModel } from "./rule.model";
 
 const conditions = ["and", "or"] as const;
 export type Condition = (typeof conditions)[number];
 
 export class QueryRuleModel extends RuleModel {
+    @IsString()
     @ApiProperty({ type: () => String })
     id!: string;
 }
 
 @ApiExtraModels(QueryRuleModel)
 export class QueryModel {
+    @IsIn(conditions)
     @ApiProperty({ enum: conditions, enumName: "Condition" })
     condition!: Condition;
 
+    @IsArray()
     @ApiProperty({
         oneOf: [
             { type: "array", items: { $ref: getSchemaPath(QueryRuleModel) } },
