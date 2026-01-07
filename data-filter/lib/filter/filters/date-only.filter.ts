6d8fbbb4e391c1ec7de6c16632ff8e0a2b5bf617
Gabriel Beaudry <gabriel.bdry@gmail.com>
2026-01-07 13:59:30 -0500

    Implémentation du date-only filter afin de limiter la comparaison à la date seulement, sans les heures.

--- data-filter/lib/filter/filters/date-only.filter.ts
+++ data-filter/lib/filter/filters/date-only.filter.ts
@@ -1,11 +1,36 @@
-import { DateFilter } from "./date.filter";
-import { BaseFilterDefinition } from "./filter";
+import { format, parseISO } from "date-fns";
+import { WhereOptions } from "sequelize";
+import { QueryRuleModel } from "../models";
+import { DateOperators, FilterOperatorTypes } from "../operators";
+import { BaseFilterDefinition, Filter } from "./filter";
+import { FilterType } from "../type";
 
-export class DateOnlyFilter extends DateFilter {
+export class DateOnlyFilter extends Filter  {
+    public type = FilterType.Date;
+    public operators = [...DateOperators];
+
     constructor(definition: BaseFilterDefinition) {
-        super({
-            ...definition,
-            skipTimezone: true
+        super(definition);
+    }
+
+    public override async getWhereOptions(rule: QueryRuleModel): Promise<WhereOptions | undefined> {
+        if (rule.operation === FilterOperatorTypes.Between || rule.operation === FilterOperatorTypes.NotBetween) {
+            if (!Array.isArray(rule.value)) {
+                return;
+            }
+
+            return super.getWhereOptions({
+                ...rule,
+                value: [format(parseISO(rule.value[0]), "yyyy-MM-dd"), format(parseISO(rule.value[1]), "yyyy-MM-dd")],
+            });
+        }
+
+        const parsedValue = format(parseISO(rule.value as any), "yyyy-MM-dd");
+
+        return super.getWhereOptions({
+            operation: rule.operation,
+            value: parsedValue,
+            id: rule.id
         });
     }
 }
